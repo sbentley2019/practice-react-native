be01793b9604bb2db1daef7a8e5aa98d5dc8f58f
sbentley2019 <sbentley000@gmail.com>
2020-02-06 15:45:55 -0500

    Add item input and onPress events

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+  Button
+} from "react-native";
 
 export default function App() {
   const [state, setState] = useState([
@@ -9,23 +18,39 @@ export default function App() {
     { name: "delta", id: 4 },
     { name: "echo", id: 5 },
     { name: "foxtrot", id: 6 },
-    { name: "golden", id: 7 }
+    { name: "golden", id: 7 },
+    { name: "helix", id: 8 }
   ]);
+  let id = state.length;
+  const [newText, setNewText] = useState("");
+
+  const handleButton = function() {
+    setState(prev => prev.concat({ name: newText, id: ++id }));
+    setNewText("");
+  };
+
+  const handleClick = function(id) {
+    setState(prev => prev.filter(x => x.id != id));
+  };
 
   return (
     <View style={styles.container}>
-      {/* <ScrollView>
-        {state.map(item => (
-          <View key={item.key}>
-            <Text style={styles.itemContainer}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter a name"
+          onChangeText={text => setNewText(text)}
+          value={newText}
+        />
+        <Button title="Submit" onPress={() => handleButton()} />
+      </View>
       <FlatList
         keyExtractor={item => item.id}
         data={state}
         renderItem={({ item }) => (
-          <Text style={styles.itemContainer}>{item.name}</Text>
+          <TouchableOpacity onPress={() => handleClick(item.id)}>
+            <Text style={styles.itemContainer}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
     </View>
@@ -35,17 +60,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
-
     justifyContent: "center"
   },
   yellow: {
     fontSize: 40
   },
   itemContainer: {
+    backgroundColor: "yellow",
     paddingVertical: 50,
     textAlign: "center",
     borderWidth: 1,
     borderColor: "black"
+  },
+  input: {
+    height: 50,
+    marginTop: 25
   }
 });
