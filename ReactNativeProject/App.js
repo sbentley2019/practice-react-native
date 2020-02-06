820267978624f810978a90091253d6107f2a80f3
sbentley2019 <sbentley000@gmail.com>
2020-02-06 13:41:29 -0500

    Practice ScrollView and FlatList

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import Message from "./components/Message";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 
 export default function App() {
-  const [state, setState] = useState("one");
+  const [state, setState] = useState([
+    { name: "alpha", key: 1 },
+    { name: "bravo", key: 2 },
+    { name: "charlie", key: 3 },
+    { name: "delta", key: 4 },
+    { name: "echo", key: 5 },
+    { name: "foxtrot", key: 6 },
+    { name: "golden", key: 7 }
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.yellow}>{state}</Text>
-      {/* <Message
-        style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
-        press={() => {
-          setState("one");
-        }}
-        name={"one"}
-      />
-      <Message
-        style={{ width: 100, height: 100, backgroundColor: "skyblue" }}
-        press={() => {
-          setState("two");
-        }}
-        name={"two"}
-      />
-      <Message
-        style={{ width: 150, height: 150, backgroundColor: "steelblue" }}
-        press={() => {
-          setState("three");
-        }}
-        name={"three"}
-      /> */}
-      <TextInput
-        placeholder="Input Text!!"
-        onChangeText={text => setState(text)}
-        value={state}
+      {/* <ScrollView>
+        {state.map(item => (
+          <View key={item.key}>
+            <Text style={styles.itemContainer}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+      <FlatList
+        data={state}
+        renderItem={({ item }) => (
+          <Text style={styles.itemContainer}>{item.name}</Text>
+        )}
       />
     </View>
   );
@@ -42,10 +35,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "yellow",
-    alignItems: "center",
+
     justifyContent: "center"
   },
   yellow: {
     fontSize: 40
+  },
+  itemContainer: {
+    paddingVertical: 50,
+    textAlign: "center",
+    borderWidth: 1,
+    borderColor: "black"
   }
 });
